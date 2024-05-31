02790d3a048383a0df8b2a2f026829650faf2414
shukronaasqarova <shukronaasqarova94@gmail.com>
2024-05-31 23:29:56 +0500

    end

---
+++ index.js
@@ -0,0 +1,156 @@
+// 1 masala
+
+// alert("foydalanuvchi kiritgan oy raqamiga mos ravishda oy nomini chiqaruvchi funksiya tuzing");
+// function month () {
+//     let oy = 8;
+// switch (oy) {
+//   case 1:
+//     console.log("Yanvar");
+//     break;
+//   case 2:
+//     console.log("Fevral");
+//     break;
+//   case 3:
+//     console.log("Mart");
+//     break;
+//   case 4:
+//     console.log("Aprel");
+//     break;
+//   case 5:
+//     console.log("May");
+//     break;
+//   case 6:
+//     console.log("Iyun");
+//     break;
+//   case 7:
+//     console.log("Iyul");
+//     break;
+//   case 8:
+//     console.log("Avgust");
+//     break;
+//   case 9:
+//     console.log("Sentabr");
+//     break;
+//   case 10:
+//     console.log("Oktabr");
+//     break;
+//   case 11:
+//     console.log("Noyabr");
+//     break;
+//   case 12:
+//     console.log("Dekabr");
+//     break;
+
+//   default:
+//     console.log("bunday oy mavjud emas");
+//     break;
+// }
+// }
+
+// month();
+
+
+// 2 masala
+
+// function price() {
+//   let a = +prompt("ballni kiriting");
+//   switch (true) {
+//     case a >= 90 && a <= 100:
+//       console.log("A");
+//       break;
+//     case a >= 80 && a < 90:
+//       console.log("B");
+//       break;
+//     case a > +70 && a < 80:
+//       console.log("C");
+//       break;
+//     case a >= 60 && a < 70:
+//       console.log("D");
+//       break;
+//     case a >= 0 && a < 60:
+//       console.log("F");
+//       break;
+//     default:
+//       break;
+//       return a;
+//   }
+// }
+
+// price();
+
+
+// 3 - masala;
+
+// function time() {
+//   let v = +prompt("vaqt kiriting");
+
+//   switch (true) {
+//     case v > 5 && v <= 12:
+//       console.log("xayrli tong");
+//       break;
+//     case v > 12 && v <= 17:
+//       console.log("xayrli kun");
+//       break;
+//     case v > 17 && v <= 21:
+//       console.log("xayrli kech");
+//       break;
+//     case v > 21 && v <= 24:
+//       console.log("xayrli tun");
+//       break;
+//     default:
+//       console.log("0 dan 24 gacha son yozing");
+//       break;
+//   }
+// } 
+
+// time();
+
+
+// // 4 - masala;
+
+// let raqam = prompt("raqam yozing");
+
+// function togriRaqam() {
+//   while (true) {
+//     if (raqam == 5) {
+//       alert("togri raqamni kiritdingiz");
+//       break;
+//     } else {
+//       raqam = prompt("5 raqamini yozing");
+//     }
+//   }
+// }
+
+// togriRaqam();
+
+// 5 - masala;
+
+// function name() {
+//   let a = 20;
+//   let i = 1;
+//   let sum = 0;
+//   while (i <= a) {
+//     if (i % 2 == 0){
+//         console.log(i);
+//         i += 2;
+//     }
+//   }
+// }
+// name();
+
+// 6 - masala;
+
+// let soz = prompt("soz yozing");
+
+// function togriSoz() {
+//   while (true) {
+//     if (soz === "stop") {
+//       alert ("stop sozini yozdingiz");
+//       break;
+//     } else {
+//       soz = prompt("stop sozini yozing");
+//     }
+//   }
+// }
+
+// togriSoz()
